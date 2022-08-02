21581a031dbb0ef40f707d405d1ad19b31b93ec4
MTmtEmpty <44959800+MatthewTao@users.noreply.github.com>
2022-08-02 23:35:17 +1000

    pyautogui methods

---
+++ pyautogui/common_methods.py
@@ -0,0 +1,30 @@
+from pyscreeze import ImageNotFoundException
+
+import pyautogui
+import time
+
+def find_button(path_to_image, confidence=0.9, delay=0.5, attempts=5):
+    buttonx = None
+    buttony = None
+    tries = 0
+    while buttonx is None:
+        try:
+            location = pyautogui.locateOnScreen(path_to_image, confidence=confidence)
+            buttonx, buttony = pyautogui.center(location)
+        except ImageNotFoundException:
+            # If button can't be found, wait a bit
+            time.sleep(delay)
+            tries += 1
+            if tries > attempts:
+                break
+        else:
+            # Button is found, break and return
+            break
+
+    if buttonx is None:
+        raise Exception('Button not found')
+    return buttonx, buttony
+
+def click_button(path_to_image):
+    x, y = find_button(path_to_image)
+    pyautogui.click(x, y)
