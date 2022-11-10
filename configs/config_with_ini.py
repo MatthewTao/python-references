87790488fdb694e2bc60c36e57f7de5c3b2b6f08
MTmtEmpty <44959800+MatthewTao@users.noreply.github.com>
2022-11-10 21:12:22 +1000

    some examples for reading config files

---
+++ configs/config_with_ini.py
@@ -0,0 +1,10 @@
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+print(config)
+print(config['DEFAULT'])
+for line in config['DEFAULT']:
+    print(f'The key of the config: {line}')
+    print(f'The value of the config: {config["DEFAULT"][line]}\n')
