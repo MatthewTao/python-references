228525944a308c599e574206710af7f1e46e939b
MTmtEmpty <44959800+MatthewTao@users.noreply.github.com>
2022-12-24 00:12:09 +1000

    config with yml

---
+++ configs/config_with_yaml.py
@@ -0,0 +1,18 @@
+"""
+Example of reading from a yaml config file
+"""
+import yaml
+
+
+def read_yaml_config():
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
+if __name__ == '__main__':
+    test_config = read_yaml_config()
+    print(test_config)
+    print(f'Config is now a dict: {test_config.get("high_level_heading_1")}')
+    print(f'Config is now a dict: {test_config["high_level_heading_1"]["key1"]}')
