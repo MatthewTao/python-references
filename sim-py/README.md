28e169d924ba059667ff17c5267a64867537cf01
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2023-07-24 17:33:26 +1000

    simpy initial init

---
+++ sim-py/README.md
@@ -0,0 +1,11 @@
+# Sim-Py
+
+An interesting tool to simulate discrete events
+
+can be similar to asyncIO, tornado
+Environment <-> Event loop
+Process <-> Task or coroutine
+Event <-> Future/Promise/Deffered
+Resources <-> Semaphore
+
+Core idea is to use Python Generators to model process.
---
+++ sim-py/basic_clock_example.py
@@ -0,0 +1,21 @@
+import simpy
+from simpy import Environment
+
+
+def clock(env: Environment, name, tick):
+    while True:
+        print(name, env.now)
+        yield env.timeout(tick)
+
+
+if __name__ == '__main__':
+    # Environment runs the simulation as fast as possible
+    env = simpy.Environment()
+
+    # Realtime Environment runs the step in inline with real time
+
+    env.process(clock(env, 'fast', 0.5))
+    env.process(clock(env, 'slow', 1))
+
+    # Interesting that the step doesn't need to be an integer
+    env.run(until=2.1)
