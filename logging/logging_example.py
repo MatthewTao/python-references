8da4378b48968c220533fb84bf4f52c00e90e2c3
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2023-12-21 15:37:09 +1000

    Update logging_example.py

--- logging/logging_example.py
+++ logging/logging_example.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 import logging
+from logging import Handler
+from sqlite3 import connect
 
 
 def main():
@@ -21,7 +24,7 @@ def more_complex_example():
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(asctime)s %(levelname)s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
 
@@ -33,7 +36,7 @@ def logging_to_file():
         level=logging.DEBUG,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        filename="basic.log"
+        filename="basic.log",
     )
 
 
@@ -45,34 +48,33 @@ def logging_to_file_and_console():
         level=logging.DEBUG,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        handlers=[
-            logging.FileHandler("basic.log"),
-            logging.StreamHandler()
-        ]
+        handlers=[logging.FileHandler("basic.log"), logging.StreamHandler()],
     )
 
 
 def logging_to_external_service():
     from logging.handlers import SysLogHandler
+
     # There are many log handlers that can send logs to different places
     # Custom log handlers can also be defined
-    
+
     # There are a whole range of different hosts
-    HOST = 'logs.papertrailapp.com'  # Dummy host
-    PORT = 37554
+    host = "logs.papertrailapp.com"  # Dummy host
+    port = 37554
 
     # The basic logger will no longer be applicable here.
     logger = logging.getLogger("example_logs")
     logger.setLevel(logging.DEBUG)
-    handler = SysLogHandler(address=(HOST, PORT))
+    handler = SysLogHandler(address=(host, port))
     logger.addHandler(handler)
     # Not restricted to one handler as well
     # Can add as many as you want.
 
     # Be extra careful what is being logged when sending to external destinations.
     # Logs can contain sensitive data.
-    # Or DON'T log sensitve data all together.
+    # Or DON'T log sensitive data all together.
     # Never assume log files are secure
+
 
 def formatters():
     # Create a custom logger
@@ -80,16 +82,46 @@ def formatters():
 
     # Create handlers
     c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler('file.log')
+    f_handler = logging.FileHandler("file.log")
     c_handler.setLevel(logging.WARNING)
     f_handler.setLevel(logging.ERROR)
 
     # Create formatters and add it to handlers
-    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+    f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     c_handler.setFormatter(c_format)
     f_handler.setFormatter(f_format)
 
     # Add handlers to the logger
     logger.addHandler(c_handler)
     logger.addHandler(f_handler)
+
+
+class SqliteHandler(Handler):
+    """
+    Example SQLite Log Handler
+    """
+    def __init__(self, db_file):
+        super().__init__()
+        self.db_file = db_file
+        self.db_file = connect(self.db_file)
+        self.db_file.execute(
+            "CREATE TABLE IF NOT EXISTS logs (date TEXT, "
+            "time TEXT, lvl INTEGER, lvl_name TEXT, msg TEXT, "
+            "logger TEXT, lineno INTEGER)"
+        )
+
+    def emit(self, record):
+        self.db_file.execute(
+            "INSERT INTO logs VALUES (:1,:2,:3,:4, :5, :6, :7)",
+            (
+                datetime.now().strftime("%A, the %d of %B, %Y"),
+                datetime.now().strftime("%I:%M %p"),
+                record.levelno,
+                record.levelname,
+                record.msg,
+                record.name,
+                record.lineno,
+            ),
+        )
+        self.db_file.commit()
