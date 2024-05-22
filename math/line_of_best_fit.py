1b1b6e84d046a6dbafa66f04e5e836ecb22ece83
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-05-22 16:35:00 +1000

    Update line_of_best_fit.py

--- math/line_of_best_fit.py
+++ math/line_of_best_fit.py
@@ -20,6 +20,19 @@ def linear_line_of_best_fit_new(x, y):
     return m, c
 
 
+def raw_python_best_fit(x, y):
+    xbar = sum(x) / len(x)
+    ybar = sum(y) / len(y)
+    n = len(x)
+
+    numer = sum([xi * yi for xi, yi in zip(x, y)]) - n * xbar * ybar
+    denum = sum([xi**2 for xi in x]) - n * xbar**2
+
+    m = numer / denum
+    c = ybar - m * xbar
+    return m, c
+
+
 def generate_random_linear_data():
     np.random.seed(0)
 
@@ -35,9 +48,14 @@ if __name__ == "__main__":
 
     gradient_new, intercept_new = linear_line_of_best_fit_new(x, y)
 
+    gradient_raw, intercept_raw = raw_python_best_fit(x, y)
+
     print(f"Gradient is {gradient} | Intercept is {intercept}")
     print(
         f"Using the new method: Gradient is {gradient_new} | Intercept is {intercept_new}"
+    )
+    print(
+        f"Using raw method: Gradient is {gradient_raw} | Intercept is {intercept_raw}"
     )
 
     # Create a scatter plot of the original data
