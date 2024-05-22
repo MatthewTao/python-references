526fecf4b570c7ec6aafd6d88d22a7ae509519ae
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-05-22 16:24:20 +1000

    Update line_of_best_fit.py

--- math/line_of_best_fit.py
+++ math/line_of_best_fit.py
@@ -9,6 +9,17 @@ def linear_line_of_best_fit(x, y):
     return m, c
 
 
+def linear_line_of_best_fit_new(x, y):
+    """
+    There is a newer method that is supposed to be more feature-ful
+    It's just got some small differences
+    """
+    p_fitted = np.polynomial.Polynomial.fit(x, y, deg=1)
+    c, m = p_fitted.convert().coef
+
+    return m, c
+
+
 def generate_random_linear_data():
     np.random.seed(0)
 
@@ -22,7 +33,12 @@ if __name__ == "__main__":
     x, y = generate_random_linear_data()
     gradient, intercept = linear_line_of_best_fit(x, y)
 
+    gradient_new, intercept_new = linear_line_of_best_fit_new(x, y)
+
     print(f"Gradient is {gradient} | Intercept is {intercept}")
+    print(
+        f"Using the new method: Gradient is {gradient_new} | Intercept is {intercept_new}"
+    )
 
     # Create a scatter plot of the original data
     plt.scatter(x, y)
