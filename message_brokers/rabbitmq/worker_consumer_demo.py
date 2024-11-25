109ce05aa44f6c8f657e7d4f61fbebd5b402e8ad
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-11-25 23:35:23 +1000

    update worker rabbitmq demo

--- message_brokers/rabbitmq/worker_consumer_demo.py
+++ message_brokers/rabbitmq/worker_consumer_demo.py
@@ -1,35 +1,58 @@
 import datetime as dt
+import json
 import time
 import pika, sys, os
+
+
+class Worker:
+    def __init__(self, channel):
+        self.channel = channel
+        channel.queue_declare(queue="task_queue", durable=True)
+        channel.queue_declare(queue="task_result", durable=True)
+
+    def run(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(
+            queue="task_queue", on_message_callback=self._callback, auto_ack=False
+        )
+
+        print(
+            f" {dt.datetime.now().isoformat()} Waiting for messages. To exit press CTRL+C"
+        )
+        self.channel.start_consuming()
+
+    def _callback(self, ch, method, properties, body: bytes):
+        decoded_message = json.loads(body.decode())
+
+        print(f" {dt.datetime.now().isoformat()} Received {decoded_message}")
+
+        # Count the number of . in the message and use sleep to simulate processing
+        time.sleep(decoded_message.get("task_duration"))
+
+        print(f" {dt.datetime.now().isoformat()} Task {decoded_message.get("task_id")} is now processed")
+
+        # Reply and acknowledge that message is successfully processed and can be deleted
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        # Send message with results
+        self._send_result(ch, json.dumps({"task_id": decoded_message.get("task_id"), "status": "success"}))
+
+    def _send_result(self, channel, message):
+        channel.basic_publish(
+            exchange="",
+            routing_key="task_result",
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent),
+        )
+        print(f" {dt.datetime.now().isoformat()} Sent {message}")
 
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
-    channel.queue_declare(queue="task_queue", durable=True)
-
-    def callback(ch, method, properties, body: bytes):
-        decoded_message = body.decode()
-        print(f" {dt.datetime.now().isoformat()} Received {decoded_message}")
-
-        # Count the number of . in the message and use sleep to simulate processing
-        time.sleep(decoded_message.count(".") * 2)
-
-        print(f" {dt.datetime.now().isoformat()} {decoded_message} is now processed")
-
-        # Reply and acknowledge that message is successfully processed and can be deleted
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(
-        queue="task_queue", on_message_callback=callback, auto_ack=False
-    )
-
-    print(
-        f" {dt.datetime.now().isoformat()} Waiting for messages. To exit press CTRL+C"
-    )
-    channel.start_consuming()
+    worker = Worker(channel)
+    worker.run()
 
 
 if __name__ == "__main__":
--- message_brokers/rabbitmq/worker_producer.py
+++ message_brokers/rabbitmq/worker_producer.py
@@ -1,12 +1,10 @@
 import datetime as dt
+import json
+import time
 import pika
 
 
-def send_message(connection, message):
-    channel = connection.channel()
-
-    channel.queue_declare(queue="task_queue", durable=True)
-
+def send_message(channel, message):
     channel.basic_publish(
         exchange="",
         routing_key="task_queue",
@@ -16,17 +14,73 @@ def send_message(connection, message):
     print(f" {dt.datetime.now().isoformat()} Sent {message}")
 
 
+def get_queue_length(channel, queue_name):
+    result = channel.queue_declare(queue=queue_name, passive=True)
+    messages = result.method.message_count
+    print(f"Queue length: {messages}")
+    return messages
+
+
+def wait_till_queue_is_empty(channel, queue_name):
+    while get_queue_length(channel, queue_name) > 0:
+        time.sleep(1)
+
+
+class ResultsReceiver:
+    def __init__(self, channel, expected_results):
+        self.channel = channel
+        self.queue_name = "task_result"
+        channel.queue_declare(queue=self.queue_name, durable=True)
+        self.results = []
+        self.expected_results = expected_results
+
+    def _callback(self, ch, method, properties, body):
+        decoded_message = json.loads(body.decode())
+        self.results.append(decoded_message)
+
+        print(f"{len(self.results)} results received")
+        if len(self.results) == self.expected_results:
+            raise StopIteration
+
+    def wait_till_all_results_received(self):
+        self.channel.basic_consume(
+            queue=self.queue_name, on_message_callback=self._callback, auto_ack=True
+        )
+        try:
+            self.channel.start_consuming()
+        except StopIteration:
+            print(f" {dt.datetime.now().isoformat()} All messages received")
+            print(f"{self.results}")
+
+        
+
 if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+    channel = connection.channel()
+    queue_name = "task_queue"
+    channel.queue_declare(queue=queue_name, durable=True)
     print("Connection opened")
 
     # Send multiple messages
-    for i in range(5):
+    for i in range(10):
         if i % 2 == 0:
-            send_message(connection, f"Task {i} ........")
+            duration = 5
         else:
-            send_message(connection, f"Small Task {i} .")
+            duration = 10
+        
+        payload = json.dumps({
+            "task_id": i,
+            "task_duration": duration
+        })
+        send_message(channel, payload)
+
+    print(f" {dt.datetime.now().isoformat()} All messages sent now")
+    receiver = ResultsReceiver(channel, 10)
+    receiver.wait_till_all_results_received()
+
+    # wait_till_queue_is_empty(channel, queue_name)
+    # print(f" {dt.datetime.now().isoformat()} All messages processed now")
+    
     connection.close()
 
     print("Connection closed now")
-    # Open up multiple worker_consumers and see how the messages get split up
