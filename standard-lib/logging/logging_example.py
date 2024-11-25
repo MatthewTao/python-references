884591d298e5f733337a10c33d08542742c13426
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-11-25 21:01:39 +1000

    Update logging_example.py

--- standard-lib/logging/logging_example.py
+++ standard-lib/logging/logging_example.py
@@ -34,7 +34,7 @@ def logging_to_file():
     """
     logging.basicConfig(
         level=logging.DEBUG,
-        format="%(asctime)s %(levelname)s %(message)s",
+        format="%(asctime)s %(name)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         filename="basic.log",
     )
