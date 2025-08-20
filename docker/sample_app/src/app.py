263591ad09fd79a0e8344d3611cf43ad90df5c24
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2025-08-20 11:01:35 +1000

    sample app use logging and keep container alive

--- docker/sample_app/src/app.py
+++ docker/sample_app/src/app.py
@@ -1,9 +1,17 @@
 """
 Entry point to whatever
 """
+import logging
 import os
+import time
 
 
 if __name__ == "__main__":
-    print("I am groot")
-    print(f"Read from env: {os.getenv('SECRET_MESSAGE')}")
+    logging.basicConfig(level=logging.INFO)
+    while True:
+        logging.info("I am groot")
+        logging.info(f"Read from env: {os.getenv('SECRET_MESSAGE')}")
+
+        # Silly loop to keep the container alive.
+        # Therefore, the container can be explored interactively
+        time.sleep(5)
